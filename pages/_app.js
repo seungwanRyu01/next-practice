42bb8d2b68df5c9f93a1f334d89974a61e6ba09c
swryu2 <swryu@goldenplanet.co.kr>
2022-05-20 10:54:09 +0900

    add :: _app.js 주석 추가

--- pages/_app.js
+++ pages/_app.js
@@ -10,3 +10,12 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+
+/**
+ * 페이지 전환시 레이아웃을 유지할 수 있습니다.
+ * 페이지 전환시 상태값을 유지할 수 있습니다.
+ * componentDidCatch를 이용해서 커스텀 에러 핸들링을 할 수 있습니다.
+ * 추가적인 데이터를 페이지로 주입시켜주는 게 가능합니다.
+ * 글로벌 CSS를 이곳에 선언합니다.
+ */
